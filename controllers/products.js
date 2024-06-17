bd051ddbbbc065d0b4ee94ae331ac638887cd8a9
iamVedanta <callvedanta@gmail.com>
2024-06-17 16:34:26 +0530

    Lec-6 getting the data from mongodb db using the find method

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,16 @@
+const Product = require("../models/product");
+
 const getAllProducts = async(req,res)=>{
-    res.status(200).json({msg:"I am get all products"});
+
+    const myData = await Product.find({});//getting all the data from the database
+    console.log(res);
+    console.log(myData);
+    res.status(200).json(myData);
 };
 
 const getProductsTesting = async(req,res)=>{
-    res.status(200).json({msg:"I am get all products testing"});
+    const myData = await Product.find({});
+    res.status(200).json(myData);
 };
 
 module.exports = {getAllProducts,getProductsTesting}
